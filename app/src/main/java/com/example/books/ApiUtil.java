be8e09bc557ca95ee883c1b6ef9248aea059aec6
Emeka <ilozuechukwuemeka@yahoo.com>
2020-07-02 00:01:40 +0100

    initial Implementation HttpConnection to book api

--- app/src/main/java/com/example/books/ApiUtil.java
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -1,8 +1,12 @@
 package com.example.books;
 
+import android.util.Log;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class ApiUtil {
     private ApiUtil(){}
@@ -26,6 +30,25 @@ public class ApiUtil {
 
     public static  String getJson(URL url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasData = scanner.hasNext();
+            if(hasData){
+                return scanner.next();
+            } else {
+                return null;
+            }
+
+        } catch (Exception e){
+            Log.d("Error", e.toString());
+            return null;
+        } finally {
+            connection.disconnect();
+        }
+
 
 
     }
--- app/src/main/java/com/example/books/BookListActivity.java
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -3,6 +3,10 @@ package com.example.books;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class BookListActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        TextView tvResponse = (TextView) findViewById(R.id.tvResponse);
+
+        try {
+            URL bookUrl = ApiUtil.buildUrl("cooking");
+            String jsonResult = ApiUtil.getJson(bookUrl);
+            tvResponse.setText(jsonResult);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
