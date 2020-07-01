a99648f6337428ae656b13decb26302aae0c7991
Emeka <ilozuechukwuemeka@yahoo.com>
2020-07-02 00:24:20 +0100

    Implementing a BooksQueryTask that extends AsyncTask

--- app/src/main/java/com/example/books/BookListActivity.java
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -1,9 +1,11 @@
 package com.example.books;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,14 +16,33 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
-        TextView tvResponse = (TextView) findViewById(R.id.tvResponse);
 
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
-            String jsonResult = ApiUtil.getJson(bookUrl);
-            tvResponse.setText(jsonResult);
-        } catch (IOException e) {
+            new BooksQueryTask().execute(bookUrl);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-}
+
+    public class BooksQueryTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchURL = urls[0];
+            String result = null;
+            try {
+                result = ApiUtil.getJson(searchURL);
+            } catch (IOException e){
+                Log.e("Error", e.getMessage());
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView tvResponse = (TextView) findViewById(R.id.tvResponse);
+            tvResponse.setText(result);
+        }
+    }
+ }
